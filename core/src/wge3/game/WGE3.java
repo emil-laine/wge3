430ed5d248188225bdc29771ce22568360635aba
Emil Laine <laine.emil@gmail.com>
2014-06-12 18:37:58 +0300

    Deleted camera which wasn't doing anything

--- core/src/wge3/game/WGE3.java
+++ core/src/wge3/game/WGE3.java
@@ -3,25 +3,18 @@ package wge3.game;
 import wge3.gamestate.GameStateManager;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class WGE3 extends Game {
     
     public int WIDTH;
     public int HEIGHT;
     
-    public OrthographicCamera cam;
     public GameStateManager gsm;
 
     @Override
     public void create () {
         WIDTH = Gdx.graphics.getWidth();
         HEIGHT = Gdx.graphics.getHeight();
-        
-        cam = new OrthographicCamera(WIDTH, HEIGHT);
-        cam.translate(WIDTH/2, HEIGHT/2);
-        cam.update();
         
         gsm = new GameStateManager();
         gsm.setState(1);
