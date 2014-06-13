0bff4abc0e42aeda0074153351f064a1026d79ed
Emil Laine <laine.emil@gmail.com>
2014-06-13 22:34:55 +0300

    Created a LightSource class for lighting.

---
+++ core/src/wge3/entity/object/LightSource.java
@@ -0,0 +1,48 @@
+package wge3.entity.object;
+
+import wge3.world.MapObject;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import wge3.world.Area;
+
+public class LightSource extends MapObject {
+    
+    private Area area;
+    
+    private Color color;
+    private int range;
+
+    public LightSource() {
+        this(5);
+    }
+    
+    public LightSource(int range) {
+        texture = new Texture(Gdx.files.internal("graphics/streetlight.png"));
+        sprite = new Sprite(texture);
+        color = new Color(1f, 1f, 1f, 1f);
+        blocksVision = false;
+        this.range = range;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getRange() {
+        return range;
+    }
+}
