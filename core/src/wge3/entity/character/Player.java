55925dbc6b97bbf1ddc1d038969197e64dd30266
Emil Laine <laine.emil@gmail.com>
2014-06-13 13:53:45 +0300

    Deleted an unused import.

--- core/src/wge3/entity/character/Player.java
+++ core/src/wge3/entity/character/Player.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
-import wge3.world.Area;
 
 public class Player extends Creature {
     
