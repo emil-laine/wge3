1913034198dbbb8ae35738728a167c1875458149
Emil Laine <laine.emil@gmail.com>
2014-06-15 02:03:26 +0300

    Unnecessary call to super constructor.

--- core/src/wge3/entity/mapobject/BrickWall.java
+++ core/src/wge3/entity/mapobject/BrickWall.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 public class BrickWall extends Wall {
 
     public BrickWall() {
-        super();
         texture = new Texture(Gdx.files.internal("graphics/brickwall.png"));
         sprite = new Sprite(texture);
     }
