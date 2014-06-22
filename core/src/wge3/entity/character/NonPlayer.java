0491486ebb6e4817b4cd368a1e9d54353db415ac
Emil Laine <laine.emil@gmail.com>
2014-06-23 01:11:14 +0300

    NPCs now have an attackSpeed to prevent them attacking FPS times in a second.

--- core/src/wge3/entity/character/NonPlayer.java
+++ core/src/wge3/entity/character/NonPlayer.java
@@ -5,13 +5,19 @@ import wge3.game.ai.AI;
 public abstract class NonPlayer extends Creature {
     
     protected AI ai;
+    protected int attackSpeed; // How many times the creature attacks in 5 seconds.
     
     public NonPlayer() {
         team = 1;
         ai = new AI(this);
+        attackSpeed = 10;
     }
     
     public void updateAI() {
         ai.update();
+    }
+    
+    public int getAttackSpeed() {
+        return attackSpeed;
     }
 }
