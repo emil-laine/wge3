dd0b18123b0ba02e6b65d3c15a460e21a0378249
Emil Laine <laine.emil@gmail.com>
2014-07-01 02:20:34 +0300

    Lava is now less deadly.

--- core/src/wge3/entity/ground/Lava.java
+++ core/src/wge3/entity/ground/Lava.java
@@ -10,7 +10,7 @@ public final class Lava extends Ground {
         sprite = new Sprite(texture, 3*Tile.size, 0, Tile.size, Tile.size);
         
         drainsHP = true;
-        HPDrainAmount = 20;
+        HPDrainAmount = 15;
         movementModifier = 0.5f;
     }
 }
