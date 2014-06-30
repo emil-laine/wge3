c6e7a603be14b8982ab05cd0b2fe7c1a74f3b272
Emil Laine <laine.emil@gmail.com>
2014-06-30 22:41:39 +0300

    Gun test

--- core/src/wge3/world/Area.java
+++ core/src/wge3/world/Area.java
@@ -51,7 +51,7 @@ public final class Area implements Drawable {
         
         mapLoader = new MapLoader();
         try {
-            mapLoader.loadMap("Untitled", this);
+            mapLoader.loadMap("PvP1", this);
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Area.class.getName()).log(Level.SEVERE, null, ex);
         }
