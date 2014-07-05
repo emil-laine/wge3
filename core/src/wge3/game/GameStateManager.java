18457c4b0ac11f87f47975b167179eb49c6bca09
Emil Laine <laine.emil@gmail.com>
2014-07-05 17:57:56 +0300

    GSM now has a variable representing the next map.

--- core/src/wge3/game/GameStateManager.java
+++ core/src/wge3/game/GameStateManager.java
@@ -5,8 +5,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public final class GameStateManager {
     
     private GameState currentState;
-    
     private SpriteBatch batch;
+    
+    private String nextMap;
 
     public GameStateManager() {
         batch = new SpriteBatch();
@@ -23,13 +24,17 @@ public final class GameStateManager {
                 break;
                 
             case 1:
-                currentState = new PlayState(this);
+                currentState = new PlayState(this, nextMap);
                 break;
                 
             case 2:
                 // currentState = new PreferencesState(this);
                 break;
         }
+    }
+
+    public void setNextMap(String nextMap) {
+        this.nextMap = nextMap;
     }
     
     public void update(float delta) {
