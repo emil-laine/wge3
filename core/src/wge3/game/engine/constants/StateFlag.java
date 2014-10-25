bf2315bfe5034126649873b712a2e5dcca94a753
Emil Laine <laine.emil@gmail.com>
2014-10-25 20:07:03 +0300

    Removed bit values because they're now obsolete as we're using EnumSet to store these StateFlags.

--- core/src/wge3/game/engine/constants/StateFlag.java
+++ core/src/wge3/game/engine/constants/StateFlag.java
@@ -1,26 +1,16 @@
 package wge3.game.engine.constants;
 
 public enum StateFlag {
-    GOING_FORWARD  (1 << 0),
-    GOING_BACKWARD (1 << 1),
-    TURNING_LEFT   (1 << 2),
-    TURNING_RIGHT  (1 << 3),
+    GOING_FORWARD,
+    GOING_BACKWARD,
+    TURNING_LEFT,
+    TURNING_RIGHT,
     
-    IS_RUNNING     (1 << 4),
-    IS_INVISIBLE   (1 << 5),
-    IS_FLYING      (1 << 6),
-    IS_GHOST       (1 << 7),
+    IS_RUNNING,
+    IS_INVISIBLE,
+    IS_FLYING,
+    IS_GHOST,
     
-    PICKS_UP_ITEMS (1 << 8),
-    SEES_EVERYTHING(1 << 9);
-    
-    private final int value;
-    
-    StateFlag(int value) {
-        this.value = value;
-    }
-    
-    public int value() {
-        return value;
-    }
+    PICKS_UP_ITEMS,
+    SEES_EVERYTHING;
 }
